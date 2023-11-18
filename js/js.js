1636699f2e00eac62db4682c19622c33790e4dff
el0ooo <eloichayer@gmail.com>
2023-11-18 13:30:19 -0500

    debut page tarif et debut son logo

--- js/js.js
+++ js/js.js
@@ -1,1 +1,11 @@
-let logoSTM = document.querySelector(".logo");
+// faire jouer le son du metro lorsque l'utilisateur click sur le logo de la stm pour revenir a la page d'acceuil
+
+let logoSTM = document.querySelector(".jouer-son");
+logoSTM.addEventListener("click", JoueurSon);
+
+// FONCTIONNE PAS POUR LE MOMENT
+function JoueurSon(){
+    let sonSTM = new Audio("../sons/sonSTM.mp3");
+    sonSTM.play();
+    console.log("jouer son");
+}
