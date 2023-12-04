fdbbeafb788db4771446e3fb88ab47658e5f9204
el0ooo <eloichayer@gmail.com>
2023-12-04 15:58:29 -0500

    essai js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // faire jouer le son du metro lorsque l'utilisateur click sur le logo de la stm pour revenir a la page d'acceuil
+let sonSTM = new Audio("js/SonSTM.mp3");
 
-let logoSTM = document.querySelector(".jouer-son");
+let logoSTM = document.querySelector(".logo");
 logoSTM.addEventListener("click", JoueurSon);
 
 // FONCTIONNE PAS POUR LE MOMENT
 function JoueurSon(){
-    let sonSTM = new Audio("../sons/sonSTM.mp3");
     sonSTM.play();
-    console.log("jouer son");
+    console.log("test");
 }
